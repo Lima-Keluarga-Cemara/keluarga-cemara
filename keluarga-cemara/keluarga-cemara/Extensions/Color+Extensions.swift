b091d45fc6d785adb1d23f38d9393748b1ba6aec
marufboy_ <afif23ixa@gmail.com>
2023-10-20 21:57:57 +0700

    chore: fix issue xcode.pbxproj

--- keluarga-cemara/keluarga-cemara/Extensions/Color+Extensions.swift
+++ keluarga-cemara/keluarga-cemara/Extensions/Color+Extensions.swift
@@ -11,4 +11,8 @@ extension Color {
     static let greyColor = Color("greyColor")
     static let greenColor = Color("primaryGreen")
     static let blackCameraColor = Color("blackCamera")
+    static let backgroundGreen = Color("backgroundGreen")
+    static let firstGradientOrange = Color("firstGradientOrange")
+    static let secondGradientOrange = Color("secondGradientOrange")
+    static let thirdGradientOrange = Color("thirdGradientOrange")
 }
