663c144868f5e221be2e8f2523cf7c514b57b946
Tiyas Aria Pratiwi <71452274+TiyasAria@users.noreply.github.com>
2023-10-29 00:49:41 +0700

    ui result

---
+++ keluarga-cemara/keluarga-cemara/RoomPlanFeature/Component/CustomSlider.swift
@@ -0,0 +1,18 @@
+//
+//  CustomSlider.swift
+//  keluarga-cemara
+//
+//  Created by tiyas aria on 28/10/23.
+//
+
+import SwiftUI
+
+struct CustomSlider: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomSlider()
+}
