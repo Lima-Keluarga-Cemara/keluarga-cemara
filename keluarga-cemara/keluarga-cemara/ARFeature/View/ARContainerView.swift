4ad66e8311125e01bde4b093ba943452743d4cda
Tiyas Aria Pratiwi <71452274+TiyasAria@users.noreply.github.com>
2023-11-14 01:43:59 +0700

    done add result scene and placing ar

--- keluarga-cemara/keluarga-cemara/ARFeature/View/ARContainerView.swift
+++ keluarga-cemara/keluarga-cemara/ARFeature/View/ARContainerView.swift
@@ -14,78 +14,131 @@ struct ARContainerView: View {
     @StateObject private var sunManager  = LocationManager()
     @EnvironmentObject private var pathStore: PathStore
     @StateObject var viewModel = ARViewModel()
+    @State private var isAlreadyPlace : Bool = false
+    @State private var isAlreadyLock : Bool = false
+    
 
     
     var body: some View {
         ZStack{
             ARViewContainerRepresentable(viewModel: viewModel)
+//            
+//            VStack{
+//                Spacer()
+//                Button {
+//                    viewModel.placeModel()
+//                } label: {
+//                    Text("Place 3d model")
+//                }
+//
+//            }
             
             VStack{
-                Spacer()
-                Button {
-                    viewModel.placeModel()
-                } label: {
-                    Text("Place 3d model")
-                }
+               if  isAlreadyLock{
+                   HStack{
+                       Spacer()
+                       if showOrientation{
+                           Text("\(sunManager.resultOrientationDirection ?? "Partial Sun")")
+                               .calloutWhite()
+                               .multilineTextAlignment(.center)
+                               .frame(width: 239, height: 67)
+                               .padding()
+                               .background(Color(.grayTextResultOrientation))
+                               .cornerRadius(14)
 
-            }
-            VStack{
-                HStack{
-                    Spacer()
-                    if showOrientation{
-                        Text("\(sunManager.resultOrientationDirection ?? "Partial Sun")")
-                            .calloutWhite()
-                            .multilineTextAlignment(.center)
-                            .frame(width: 239, height: 67)
-                            .padding()
-                            .background(Color(.grayTextResultOrientation))
-                            .cornerRadius(14)
+                       }
+                       Spacer()
+                       
+                       Menu {
+                           ButtonMenu(action: $showOrientation, title: "Garden Orientation")
+                           ButtonMenu(action: $showShadePattern, title: "Shade Pattern")
+                       } label: {
+                           Image(systemName: "line.3.horizontal.decrease.circle.fill")
+                               .colorMultiply(Color(.black))
+                               .font(.system(size: 32))
+                           
+                       }
+                       .padding(.trailing,20)
+   //                    try add frame , check clickable
+                       .frame(width: 50, height: 50)
 
-                    }
-                    Spacer()
-                    
-                    Menu {
-                        ButtonMenu(action: $showOrientation, title: "Garden Orientation")
-                        ButtonMenu(action: $showShadePattern, title: "Shade Pattern")
-                    } label: {
-                        Image(systemName: "line.3.horizontal.decrease.circle.fill")
-                            .colorMultiply(Color(.black))
-                            .font(.system(size: 32))
+                   }
+                   .padding(.top)
+
+                   Spacer()
+                   if showShadePattern{
+                       VStack(alignment: .leading){
+                           HStack{
+                               Spacer()
+                               Text("10 AM")
+                                   .font(.system(size: 16, weight: .semibold, design: .rounded))
+                                   .bold()
+                               Spacer()
+                           }
+                                           
+                           Slider(value: $sliderValue, in: 0...10)
+                           .tint(.yellow)
+                           
+                       }
+                       .foregroundColor(.white)
+                       .frame(height: 53)
+                       .padding()
+                       .background(Color(.colorGraySlider))
+                       .cornerRadius(16)
+                       .padding(.horizontal,30)
+                       .padding(.bottom,20 )
+                   }
+               } else {
+                   VStack{
+                       Spacer()
+                       if !isAlreadyLock && isAlreadyPlace {
+                           Text("Place the 3D model in accordance\nwith every corner in your garden")
+                               .calloutWhite()
+                               .multilineTextAlignment(.center)
+                               .padding(.horizontal, 20)
+                               .padding(.vertical, 10)
+                               .background(Color(.black).opacity(0.7))
+                               .cornerRadius(12)
+                               .padding(.bottom,80)
+                       }
+                       HStack{
+                           Spacer()
+                           
+                           Button {
+                               viewModel.placeModel()
+                               isAlreadyPlace.toggle()
+                           } label: {
+                               Text("Place 3d Model")
+                                   .titleButton()
+                           }
+                           .padding(.horizontal, 11)
+                           .padding(.vertical,12)
+                           .background(Color(.black))
+                           .cornerRadius(12)
+                           .disabled(isAlreadyPlace)
+
+                           
                         
-                    }
-                    .padding(.trailing,20)
-//                    try add frame , check clickable
-                    .frame(width: 50, height: 50)
+                           
+                           Button {
+                               isAlreadyLock.toggle()
+                           } label: {
+                               Text("Lock 3d Model")
+                                   .titleButton()
+                           }
+                           .disabled(!isAlreadyPlace)
+                           .padding(.horizontal, 11)
+                           .padding(.vertical,12)
+                           .background(Color(.black))
+                           .cornerRadius(12)
+                           
+                           Spacer()
 
-                }
-                .padding(.top)
-
-                
-                
-
-                Spacer()
-                if showShadePattern{
-                    VStack(alignment: .leading){
-                        HStack{
-                            Spacer()
-                            Text("10 AM")
-                                .font(.system(size: 16, weight: .semibold, design: .rounded))
-                                .bold()
-                            Spacer()
-                        }
-                                        
-                        Slider(value: $sliderValue, in: 0...10)
-                        .tint(.yellow)
-                        
-                    }
-                    .foregroundColor(.white)
-                    .frame(height: 53)
-                    .padding()
-                    .background(Color(.colorGraySlider))
-                    .cornerRadius(16)
-                    .padding(.horizontal,30)
-                    .padding(.bottom,20 )
-                }
+                       }
+                   }
+                   .padding(.bottom, 25)
+               }
+               
             }
         }
         .toolbar{
--- keluarga-cemara/keluarga-cemara/ResultFeature/Controller/ObjViewController.swift
+++ keluarga-cemara/keluarga-cemara/ResultFeature/Controller/ObjViewController.swift
@@ -26,26 +26,26 @@ struct SceneKitViewAll: UIViewRepresentable {
         let orientations = getXYZOrientation()
         let firstOrientation = orientations[0]
         
-
         
-           
+        
+        
         UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.5, delay: 0, options: .curveEaseInOut) {
-               uiView.scene?.rootNode.childNode(withName: "Light", recursively: false)?.eulerAngles = SCNVector3(firstOrientation.x, firstOrientation.y, firstOrientation.z)
-              
-           }
-
-           uiView.antialiasingMode = .multisampling4X
-           uiView.autoenablesDefaultLighting = true
-           uiView.allowsCameraControl = true
- 
-
+            uiView.scene?.rootNode.childNode(withName: "Light", recursively: false)?.eulerAngles = SCNVector3(firstOrientation.x, firstOrientation.y, firstOrientation.z)
+            
+        }
+        
+        uiView.antialiasingMode = .multisampling4X
+        uiView.autoenablesDefaultLighting = true
+        uiView.allowsCameraControl = true
+        
+        
     }
     
     
-
+    
     func getXYZOrientation() -> [SCNVector3] {
         var orientations: [SCNVector3] = []
-
+        
         for i in 0..<6 {
             let x = lightPosition.orientation_x[i]
             let y = lightPosition.orientation_y[i]
@@ -54,10 +54,10 @@ struct SceneKitViewAll: UIViewRepresentable {
             orientations.append(orientation)
             print(orientation)
         }
-
+        
         return orientations
     }
-
+    
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
@@ -94,26 +94,26 @@ struct SceneKitView: UIViewRepresentable {
         let firstOrientation = orientations[0]
         let secondOrientation = orientations[1]
         
-
         
-           
+        
+        
         UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.5, delay: 0, options: .curveEaseInOut) {
-               uiView.scene?.rootNode.childNode(withName: "Light", recursively: false)?.eulerAngles = SCNVector3(firstOrientation.x, firstOrientation.y, firstOrientation.z)
-              
-           }
-
-           uiView.antialiasingMode = .multisampling4X
-           uiView.autoenablesDefaultLighting = true
-           uiView.allowsCameraControl = true
- 
-
+            uiView.scene?.rootNode.childNode(withName: "Light", recursively: false)?.eulerAngles = SCNVector3(firstOrientation.x, firstOrientation.y, firstOrientation.z)
+            
+        }
+        
+        uiView.antialiasingMode = .multisampling4X
+        uiView.autoenablesDefaultLighting = true
+        uiView.allowsCameraControl = true
+        
+        
     }
     
     
-
+    
     func getXYZOrientation() -> [SCNVector3] {
         var orientations: [SCNVector3] = []
-
+        
         for i in 0..<6 {
             let x = lightPosition.orientation_x[i]
             let y = lightPosition.orientation_y[i]
@@ -122,10 +122,10 @@ struct SceneKitView: UIViewRepresentable {
             orientations.append(orientation)
             print(orientation)
         }
-
+        
         return orientations
     }
-
+    
     func makeCoordinator() -> Coordinator {
         Coordinator(self)
     }
@@ -141,9 +141,9 @@ struct SceneKitView: UIViewRepresentable {
 }
 
 struct Coordinate: Hashable {
-   var x: Double
-   var y: Double
-   var z: Double
+    var x: Double
+    var y: Double
+    var z: Double
 }
 
 
@@ -151,59 +151,94 @@ struct Coordinate: Hashable {
 
 
 struct ResultScanYogi: View {
-   @EnvironmentObject private var pathStore: PathStore
-   @State private var isLoading : Bool = true
-   @State private var selectedTime : TimeInterval = 0.0
-   @StateObject private var sunManager = LocationManager()
-   @StateObject private var locationManager = LocationManager()
-   @StateObject var lightPosition = LightPosition()
-   @State private var sliderValue: Double = 0.0
-   @State private var timer: Timer? = nil
-
+    @EnvironmentObject private var pathStore: PathStore
+    @State private var isLoading : Bool = true
+    @State private var selectedTime : TimeInterval = 0.0
+    @StateObject private var sunManager = LocationManager()
+    @StateObject private var locationManager = LocationManager()
+    @StateObject var lightPosition = LightPosition()
+    @State private var sliderValue: Double = 0.0
+    @State private var timer: Timer? = nil
+    
+    
     @State private var timeValue: [Double] = [06.00, 08.00, 10.00, 12.00, 13.00, 15.00, 17.00]
     @State private var coordinates: [Coordinate] = []
-
-   let date = Date()
-   var calendar = Calendar.current
+    
+    let date = Date()
+    var calendar = Calendar.current
     
     var selectedDate: Date? {
-           var dateComponents = calendar.dateComponents([.year, .month, .day], from: date)
-           dateComponents.hour = Int(sliderValue) // format 24 hours
-           return calendar.date(from: dateComponents)
-       }
-
+        var dateComponents = calendar.dateComponents([.year, .month, .day], from: date)
+        dateComponents.hour = Int(sliderValue) // format 24 hours
+        return calendar.date(from: dateComponents)
+    }
+    
     var body: some View {
-        SceneKitView(lightPosition: lightPosition, scene: PhysicallyBasedScene(lightPosition: lightPosition))
-//        if sliderValue == 16.00{
-//            SceneKitViewAll(lightPosition: lightPosition, scene: PhysicallyBasedScene(lightPosition: lightPosition))
-//
-//        }
-
-        if let selectedDate = selectedDate {
-            let selectedDatePlus30Minutes = calendar.date(byAdding: .minute, value: 0, to: selectedDate)
-            Slider(value: $sliderValue, in: 7.00...16.00, step: 1)
-                .padding()
-                .onChange(of: sliderValue) { newValue in
-                    let sunPosition = locationManager.sun?.getSunHorizonCoordinatesFrom(date: selectedDatePlus30Minutes ?? Date())
-                    let sunAltitude = sunPosition?.altitude
-                    let sunAzimuth = sunPosition?.azimuth
-                    
-                    let r: Double = 1.0
-                    let theta: Double = sunAltitude?.radians ?? 0.0
-                    let phi: Double = sunAzimuth?.radians ?? 0.0
-                    
-                    let x = r * sin(theta) * cos(phi)
-                    let y = r * sin(theta) * sin(phi)
-                    let z = r * cos(theta)
-                    
-                    lightPosition.orientation_x[0] = Float(x)
-                    lightPosition.orientation_y[0] = Float(y)
-                    lightPosition.orientation_z[0] = Float(z)
-                    
+        ZStack{
+            Color.gray.ignoresSafeArea()
+            
+            VStack{
+                Text("\(sunManager.resultOrientationDirection ?? "Partial Sun")")
+                                 .calloutWhite()
+                                 .multilineTextAlignment(.center)
+                                 .frame(width: 239, height: 67)
+                                 .padding()
+                                 .background(Color(.black).opacity(0.7))
+                                 .cornerRadius(14)
+                
+                if isLoading{
+                    ProgressView()
+                        .progressViewStyle(CircularProgressViewStyle(tint: Color.brown))
+                        .scaleEffect(4)
+                        .frame( height: UIScreen.main.bounds.height / 2 )
+                } else {
+                    SceneKitView(lightPosition: lightPosition, scene: PhysicallyBasedScene(lightPosition: lightPosition))
                 }
+                //        if sliderValue == 16.00{
+                //            SceneKitViewAll(lightPosition: lightPosition, scene: PhysicallyBasedScene(lightPosition: lightPosition))
+                //
+                //        }
+                
+//                MARK: Change slidernya yaa ka yogiix, tambahin buuton buat refresh ke posisi awal juga
+                if let selectedDate = selectedDate {
+                    let selectedDatePlus30Minutes = calendar.date(byAdding: .minute, value: 0, to: selectedDate)
+                    Slider(value: $sliderValue, in: 7.00...16.00, step: 1)
+                        .padding()
+                        .onChange(of: sliderValue) { newValue in
+                            let sunPosition = locationManager.sun?.getSunHorizonCoordinatesFrom(date: selectedDatePlus30Minutes ?? Date())
+                            let sunAltitude = sunPosition?.altitude
+                            let sunAzimuth = sunPosition?.azimuth
+                            
+                            let r: Double = 1.0
+                            let theta: Double = sunAltitude?.radians ?? 0.0
+                            let phi: Double = sunAzimuth?.radians ?? 0.0
+                            
+                            let x = r * sin(theta) * cos(phi)
+                            let y = r * sin(theta) * sin(phi)
+                            let z = r * cos(theta)
+                            
+                            lightPosition.orientation_x[0] = Float(x)
+                            lightPosition.orientation_y[0] = Float(y)
+                            lightPosition.orientation_z[0] = Float(z)
+                            
+                        }
+                }
+                
+                GeneralCostumButton(title: "See shade result", action: {
+                    pathStore.navigateToView(.arview)
+                }, isShowIcon: true)
+            }
+            
+        }
+        .toolbarBackground(.black, for: .navigationBar)
+        .toolbarBackground(.visible, for: .navigationBar)
+        .onAppear {
+            DispatchQueue.global(qos: .userInitiated).asyncAfter(deadline: .now() + 2, execute: {
+                isLoading = false
+            })
         }
     }
-
+    
 }
 
 
